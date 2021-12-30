cae1cad146c26717d5c8e8f3bd05f5a46524c117
israel santucci <israelsantucci2012@gmail.com>
2021-12-30 13:34:59 -0300

    Finalizado a parte 2 do curso

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Java Exceções: Aprenda a criar, lançar e controlar exceções
 * O que é depuração (debug) e para que serve.
 * Como utilizar o Eclipse e sua perspectiva de debug.
 * Como alternar entre perspectivas do Eclipse.
+
+* O que são exceções, para que servem e porquê utilizá-las.
+* Como analisar o rastro de exceções, ou stacktrace.
+* Tratar exceções com os blocos try-catch.
+* Manipular uma exceção lançada dentro do bloco catch.
+* Tratar múltiplas exceções com mais de um bloco catch ou usando Multi-Catch utilizando o pipe (|).
--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -1,22 +1,30 @@
 public class Fluxo {
 
-    public static void main(String[] args) {
-        System.out.println("Ini do main");
-        metodo1();
-        System.out.println("Fim do main");
-    }
+	public static void main(String[] args) {
+		System.out.println("Ini do main");
+		try {
+			metodo1();	
+		}catch(ArithmeticException | NullPointerException ex) {
+			System.out.println(ex.getMessage());
+			ex.printStackTrace();
+		}
+		System.out.println("Fim do main");
+	}
 
-    private static void metodo1() {
-        System.out.println("Ini do metodo1");
-        metodo2();
-        System.out.println("Fim do metodo1");
-    }
+	private static void metodo1() {
+		System.out.println("Ini do metodo1");
+			metodo2();
+		System.out.println("Fim do metodo1");
+	}
 
-    private static void metodo2() {
-        System.out.println("Ini do metodo2");
-        for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
-        }
-        System.out.println("Fim do metodo2");        
-    }
+	private static void metodo2() {
+		System.out.println("Ini do metodo2");
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+			int a = i / 0;
+			Conta conta = null;
+			conta.deposita();
+		}
+		System.out.println("Fim do metodo2");
+	}
 }
